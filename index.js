489768641121fbdc30aa68f3f5c741d868e40f5b
Jack Boucher <jboucher56@gmail.com>
2022-06-11 13:47:26 -0500

    Added onload function to include each png

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ var opponentScore ;
 
 var choices = ["rock", "paper", "scissors"];
 
+window.onload = function() {
+    for (let i = 0; i < 3; i++) {
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        document.getElementById("choices").append(choice);
+    }
+}
