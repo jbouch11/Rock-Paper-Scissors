9a529e8d0f420d8c8033149596358b4a98acfc88
Jack Boucher <jboucher56@gmail.com>
2022-06-15 12:08:44 -0500

    score is returning NAN, will come back later

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var you;
 var yourScore;
 var opponent;
-var opponentScore ;
+var opponentScore;
 
 var choices = ["rock", "paper", "scissors"];
 
@@ -10,6 +10,49 @@ window.onload = function() {
         let choice = document.createElement("img");
         choice.id = choices[i];
         choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
         document.getElementById("choices").append(choice);
     }
+}
+
+function selectChoice() {
+    you = this.id;
+    document.getElementById("your-choice").src = you + ".png";
+
+    opponent = choices[Math.floor(Math.random()*3)];
+    document.getElementById("opponent-choice").src = opponent + ".png";
+
+    if (you == opponent) {
+        yourScore += 1;
+        opponentScore += 1;
+    }
+    else {
+        if (you == "rock") {
+            if (opponent == "scissors") {
+                yourScore += 1;
+            }
+            else if (opponent == "paper") {
+                opponentScore += 1;
+            }
+        }
+        else if (you == "scissors") {
+            if (opponent == "paper") {
+                yourScore += 1;
+            }
+            else if (opponent == "rock") {
+                opponentScore += 1;
+            }
+        }
+        else if (you == "paper") {
+            if (opponent == "rock") {
+                yourScore += 1;
+            }
+            else if (opponent == "scissors") {
+                opponentScore += 1;
+            }
+        }
+    }
+
+    document.getElementById("your-score").innerText = yourScore;
+    document.getElementById("opponent-score").innerText = opponentScore;
 }
